3cc4b1581003cb9b86c16bc6eedd7813bf84ef26
anubhav15042002 <anubhav15sharma@gmail.com>
2024-12-11 15:49:17 +0530

    my first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const itemRoutes = require('./routes/itemRoutes'); 
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const databaseUrl =process.env.DATABASE_URL;
+
+
+app.use(express.json());
+
+mongoose.connect(databaseUrl);
+
+app.use('/api/items' , itemRoutes);
+
+app.listen(port,()=>{
+    console.log('Server started on port 5000');
+});
+
+
+
+
+
+
+
